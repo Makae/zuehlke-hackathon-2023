5596f5b80f2adf9898f65772a05dd47ad8426e8a
livia <erdelyilivia@gmail.com>
2023-03-29 09:29:20 +0200

    Test if it is parallel

--- backend/src/main/java/ch/zuehlke/fullstack/hackathon/HackathonApplication.java
+++ backend/src/main/java/ch/zuehlke/fullstack/hackathon/HackathonApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import reactor.core.publisher.Mono;
 
 @EnableScheduling
 @SpringBootApplication
@@ -30,32 +31,38 @@ public class HackathonApplication implements CommandLineRunner {
         // make instances of state machine (state machine factory?)
 
         StateMachine<GameState,GameEvent> stateMachine = factory.getStateMachine("machine1");
+
         log.info("----" + stateMachine);
         stateMachine.start();
         stateMachine1(stateMachine);
 
         StateMachine<GameState,GameEvent> stateMachine2 = factory.getStateMachine("machine2");
-        stateMachine2.start();
+        Mono.just(stateMachine1(stateMachine)).zipWith(Mono.just(stateMachine2(stateMachine2))).block();
+
         stateMachine2(stateMachine2);
         log.info("----" + stateMachine2);
     }
 
-    private void stateMachine1(StateMachine<GameState, GameEvent> stateMachine) {
+    private boolean stateMachine1(StateMachine<GameState, GameEvent> stateMachine) {
+        stateMachine.start();
         //private void stateMachine1(StateMachine<GameState, GameEvent> stateMachine) {
         stateMachine.sendEvent(GameEvent.ALL_PLAYERS_JOINED);
         stateMachine.sendEvent(GameEvent.ALL_BOATS_PLACED);
         stateMachine.sendEvent(GameEvent.ATTACK);
         stateMachine.sendEvent(GameEvent.ATTACK);
         stateMachine.sendEvent(GameEvent.ALL_BOATS_DESTROYED);
+        return true;
     }
 
-    private void stateMachine2(StateMachine<GameState, GameEvent> stateMachine) {
+    private boolean stateMachine2(StateMachine<GameState, GameEvent> stateMachine) {
+        stateMachine.start();
         log.info("in stateMachine2");
         stateMachine.sendEvent(GameEvent.ALL_PLAYERS_JOINED);
         stateMachine.sendEvent(GameEvent.ALL_BOATS_PLACED);
         stateMachine.sendEvent(GameEvent.ATTACK);
         stateMachine.sendEvent(GameEvent.ATTACK);
         stateMachine.sendEvent(GameEvent.ALL_BOATS_DESTROYED);
+        return true;
     }
 
 }
